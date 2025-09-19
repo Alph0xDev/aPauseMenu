9c7a936b86b489cd8afeeb6316a867afadde761a
Alph0xDev <alph0xdev@gmail.com>
2025-09-19 20:09:25 +0200

    Add files via upload

--- server.lua
+++ server.lua
@@ -1,18 +1,18 @@
--- GitHub update tracker (check only on resource start)
+-- GitHub update tracker (check only on resource start, print release title)
 local githubRepo = "alph0xdev/aPauseMenu"
 
 PerformHttpRequest("https://api.github.com/repos/" .. githubRepo .. "/releases/latest", function(statusCode, response, headers)
     if statusCode == 200 and response then
         local data = json.decode(response)
-        if data and data.tag_name then
-            print("^2[aPauseMenu]^0 Latest GitHub release: " .. data.tag_name)
+        if data and data.name then
+            print("^2[aPauseMenu]^0 Latest GitHub release: " .. data.name)
         end
     end
 end, "GET", "", {["Content-Type"] = "application/json"})
 
 RegisterServerEvent('player:disconnect')
 AddEventHandler('player:disconnect', function()
-    local msg = Config.disconnectMessage or 'Thank you for playing on New Italy Roleplay!'
+    local msg = Config.disconnectMessage or 'Thank you for playing on Alph0xDev!'
     DropPlayer(source, msg)
 end)
 
